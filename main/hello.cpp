9d9d1d1d8925057a1bbab3c4229cfdf6d475d447
Malavika_V <malavikav.blr@gmail.com>
2024-03-09 20:01:47 +0530

    Changes to hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "This is PES2UG21CS271"
+  cout << "This is PES2UG21CS271\n";
   return 0;
 }
